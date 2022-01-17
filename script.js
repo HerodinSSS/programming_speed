b0adc3972163e6b1cc429160750716ca6d8c06f2
Allen Santos <araujosantosallen@gmail.com>
2022-01-17 17:20:52 -0500

    master

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
 
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.keyCode==9 || e.which==9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1; 
+        }
+    }
+}
 quoteInputElement.addEventListener('input', () =>  {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
     const arrayValue = quoteInputElement.value.split('')
@@ -31,14 +43,34 @@ return fetch(RANDOM_QUOTE_API_URL)
 }
 
 async function renderNewQuote() {
-    const quote = await getRandomQuote()
+    const quote = `var Ygritte = COLS
+    var Jon = dog
+    def TABLE[-82][i] {
+        if(( Hodor )){
+        if(-0.06){
+        Sansa /= TABLE[-Hodor][( x ) - -x];
+        if(protect(ROWS,x / Jon + mislead(( ( -0.13 ) )))){
+        dog /= ( 0.35 );
+        -COLS;
+        Stark += mislead(-destroy(250.97,-betray(Sansa,( ROWS ))) - ( betray() ))
+    &nbsp } else {
+        x
+    }
+    }
+    };
+        ( ( ( destroy(15 - ROWS) ) ) );
+        ( -( Ygritte ) ) / ---y * TABLE[rule(mislead(),protect(Sansa,-61))][TABLE[COLS * TABLE[x][( -( Stark * -Jon + COLS + ( ( 750.93 - -85 - ( foo() ) * ( ROWS * 1 ) ) ) ) )]][protect(Hodor)]] + ( -TABLE[mislead(ROWS) - COLS][-Ygritte] )
+    }`
     quoteDisplayElement.innerHTML = ''
     quote.split('').forEach(character => {
-        const characterSpan = document.createElement('span')
+        const characterSpan = document.createElement('span')        
         characterSpan.innerText = character
         quoteDisplayElement.appendChild(characterSpan)
     })
-    quoteInputElement.value=null
+    var childDivs = document.getElementById('quoteDisplay')
+    for(var i=0; i< childDivs.length; i++ ){
+        console.log(childDivs[i])
+    }
     startTimer()
 }
 
